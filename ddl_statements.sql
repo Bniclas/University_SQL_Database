095db9e09fdf1b2bc196a9ef5042951194610e57
Bniclas <131747708+Bniclas@users.noreply.github.com>
2024-08-09 21:24:43 +0200

    Some DDL Changes

--- ddl_statements.sql
+++ ddl_statements.sql
@@ -5,29 +5,30 @@ CREATE DATABASE university;
 USE university;
 
 CREATE TABLE IF NOT EXISTS gender (
-	gender VARCHAR(3),
-	gender_name VARCHAR( 30 ),
+	gender NVARCHAR(3),
+	gender_name NVARCHAR( 30 ),
 	PRIMARY KEY( gender )
 );
 
 CREATE TABLE IF NOT EXISTS semester(
 	semester_id BIGINT UNSIGNED AUTO_INCREMENT,
-	semster_start DATE NOT NULL,
+	semester_start DATE NOT NULL,
 	semester_end DATE NOT NULL,
-	PRIMARY KEY ( semester_id )
+	PRIMARY KEY ( semester_id ),
+	CONSTRAINT check_dates CHECK( semester_start < semester_end )
 );
 
 CREATE TABLE IF NOT EXISTS postcode (
-	postcode_code VARCHAR( 5 ),
-	postcode_city VARCHAR( 60 ) UNIQUE NOT NULL,
+	postcode_code NVARCHAR( 5 ),
+	postcode_city NVARCHAR( 60 ) UNIQUE NOT NULL,
 	PRIMARY KEY ( postcode_code )
 );
 
 CREATE TABLE IF NOT EXISTS university_locations (
 	location_id BIGINT UNSIGNED AUTO_INCREMENT,
-	location_name VARCHAR( 100 ) NOT NULL,
-	street VARCHAR( 60 ) NOT NULL,
-	fk_postcode VARCHAR(5) NOT NULL,
+	location_name NVARCHAR( 100 ) NOT NULL,
+	street NVARCHAR( 60 ) NOT NULL,
+	fk_postcode NVARCHAR(5) NOT NULL,
 	PRIMARY KEY( location_id ),
 	FOREIGN KEY ( fk_postcode ) REFERENCES postcode( postcode_code )
 );
@@ -45,12 +46,13 @@ CREATE TABLE IF NOT EXISTS timeslot (
 	slot_id BIGINT UNSIGNED AUTO_INCREMENT,
 	start_at DATETIME NOT NULL,
 	end_at DATETIME NOT NULL,
-	PRIMARY KEY( slot_id )
+	PRIMARY KEY( slot_id ),
+	CONSTRAINT check_time CHECK( start_at < end_at )
 );
 
 CREATE TABLE IF NOT EXISTS department (
 	department_id BIGINT UNSIGNED AUTO_INCREMENT,
-	department_name VARCHAR( 100 ) NOT NULL,
+	department_name NVARCHAR( 100 ) NOT NULL,
 	fk_uniloc BIGINT UNSIGNED NOT NULL,
 	PRIMARY KEY ( department_id ),
 	FOREIGN KEY ( fk_uniloc ) REFERENCES university_locations( location_id )
@@ -58,14 +60,14 @@ CREATE TABLE IF NOT EXISTS department (
 
 CREATE TABLE IF NOT EXISTS job (
 	job_id BIGINT UNSIGNED AUTO_INCREMENT,
-	job_name VARCHAR( 100 ) UNIQUE NOT NULL,
+	job_name NVARCHAR( 100 ) UNIQUE NOT NULL,
 	PRIMARY KEY ( job_id )
 );
 
 CREATE TABLE IF NOT EXISTS currency (
 	currency_id BIGINT UNSIGNED AUTO_INCREMENT,
-	currency_symbol VARCHAR(5) UNIQUE NOT NULL,
-	currency_name VARCHAR(20) UNIQUE NOT NULL,
+	currency_symbol NVARCHAR(5) UNIQUE NOT NULL,
+	currency_name NVARCHAR(20) UNIQUE NOT NULL,
 	PRIMARY KEY (currency_id)
 );
 
@@ -79,21 +81,21 @@ CREATE TABLE IF NOT EXISTS salary (
 
 CREATE TABLE IF NOT EXISTS degree (
 	degree_id BIGINT UNSIGNED AUTO_INCREMENT,
-	degree_name VARCHAR( 30 ) UNIQUE NOT NULL,
+	degree_name NVARCHAR( 30 ) UNIQUE NOT NULL,
 	PRIMARY KEY ( degree_id )
 );
 
 CREATE TABLE IF NOT EXISTS person (
 	person_id BIGINT UNSIGNED AUTO_INCREMENT,
-	gender VARCHAR(3) NOT NULL,
-	email VARCHAR( 60 ) UNIQUE NOT NULL,
-	password VARCHAR( 255 ),
-	firstname VARCHAR(60) NOT NULL,
-	surname VARCHAR(60) NOT NULL,
+	gender NVARCHAR(3) NOT NULL,
+	email NVARCHAR( 60 ) UNIQUE NOT NULL,
+	password NVARCHAR( 255 ),
+	firstname NVARCHAR(60) NOT NULL,
+	surname NVARCHAR(60) NOT NULL,
 	birthdate DATE NOT NULL,
-	postcode VARCHAR(5) NOT NULL,
-	location VARCHAR(50) NOT NULL,
-	street VARCHAR(60) NOT NULL,
+	postcode NVARCHAR(5) NOT NULL,
+	location NVARCHAR(50) NOT NULL,
+	street NVARCHAR(60) NOT NULL,
 	fk_degree BIGINT UNSIGNED,
 	PRIMARY KEY ( person_id ),
 	FOREIGN KEY ( fk_degree ) REFERENCES degree( degree_id )
@@ -122,7 +124,7 @@ CREATE TABLE IF NOT EXISTS department_staff (
 
 CREATE TABLE IF NOT EXISTS discipline (
 	discipline_id BIGINT UNSIGNED AUTO_INCREMENT,
-	discipline_name VARCHAR( 60 ) NOT NULL,
+	discipline_name NVARCHAR( 60 ) NOT NULL,
 	discipline_semesters ENUM ( "1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14" ) NOT NULL,
 	fk_prof BIGINT UNSIGNED NOT NULL,
 	fk_degree BIGINT UNSIGNED NOT NULL,
@@ -188,5 +190,6 @@ CREATE TABLE IF NOT EXISTS exam_results (
 	reached_points TINYINT UNSIGNED,
 	reached_mark DOUBLE(2,1),
 	FOREIGN KEY ( fk_exam ) REFERENCES exam( exam_nr ),
-	FOREIGN KEY ( fk_matnr ) REFERENCES student( mat_id ) 
+	FOREIGN KEY ( fk_matnr ) REFERENCES student( mat_id ),
+	CONSTRAINT check_mark CHECK( reached_mark <= 5.0 )
 );
